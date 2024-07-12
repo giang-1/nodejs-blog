f66ec17ed5bca821a8b5d71f0aa6c85c64e28f58
giang-1 <giang06121102@gmail.com>
2024-07-12 10:03:01 +0700

    done scss

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// const express = require('express')
+// const paths = require('path'); 
+// import express from 'express';
+// import { engine } from 'express-handlebars'
+// const nodemailer = require("nodemailer");
+const express = require('express');
+const handlebars = require('express-handlebars')
+const morgan = require('morgan')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+console.log(__dirname, 'public')
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}/`)
+})
