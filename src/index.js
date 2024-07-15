4831a739abd6e8e1f5e5868db63cf59a33edf363
giang-1 <giang06121102@gmail.com>
2024-07-15 21:11:22 +0700

    husky

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const route = require('./routes/index');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine(
@@ -19,7 +19,7 @@ app.engine(
     handlebars.engine({
         extname: 'hbs',
     }),
-);
+)
 app.set("view engine", 'hbs');
 app.set("views", path.join(__dirname, 'resources/views'));
 
@@ -27,4 +27,4 @@ route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}/`);
-});
+})
